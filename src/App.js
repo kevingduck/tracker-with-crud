77c0bf65a705308e41f2a210a5dedf5efa0e5ac6
Kevin Duck <kevingduck>
2020-10-12 17:39:17 -0400

    hook up form data to api

--- src/App.js
+++ src/App.js
@@ -4,40 +4,11 @@ import Form from './components/form.js';
 import Header from './components/header.js';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      data: null,
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:8888/.netlify/functions/items')
-      .then(response => response.json())
-      .then(data => this.setState({ data }))
-  }
-
-  handleClick(e) {
-    e.preventDefault()
-    console.log('clicked')
-    const contents = this.state.data
-    // const contents = this.state.data.forEach(item => {
-    //   // change the title and location key based on your API
-    //   return <p>{item}</p>
-    // })
-    for(let i=0;i<contents.length;i++){
-      console.log(contents[i].data)
-    }
-  }
-
 
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <button onClick={e => this.handleClick(e)}>load items</button>
-        {this.contents}
         <Form></Form>
       </div>
     );
--- src/components/form.js
+++ src/components/form.js
@@ -1,132 +1,57 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    // this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.objectifyForm = this.objectifyForm.bind(this);
+  }
+
+  objectifyForm(formArray) {
+    //serialize data function
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++) {
+      returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    let data = this.objectifyForm(form);
+
+    console.log('state data: ' + JSON.stringify(data));
+    fetch('./.netlify/functions/items', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  }
+
   render() {
     return (
-      <div class="container-fluid main-form">
-        <div class="row">
-          <div class="col-md-6 col-sm-6 col-xs-6">
-            <form>
-              <div class="form-group">
-                <button class="btn btn-primary" onClick={this.myFunction}>
-                  Create Project
-                </button><br></br>
-              </div>
-              <div class="form-group">
-                <label class="control-label " for="po_number">
-                  PO Number
-                    </label>
-                <input class="form-control" id="po_number" name="po_number" type="text" />
-              </div>
-              <div class="form-group ">
-                <label class="control-label" for="name">
-                  Customer Name
-                        </label>
-                <input class="form-control" id="name" name="name" type="text" />
-              </div>
-              <div class="form-group ">
-                <label class="control-label " for="representative">
-                  Representative
-                        </label>
-                <input class="form-control" id="representative" name="representative" type="text" />
-              </div>
-              <div class="form-group ">
-                <label class="control-label " for="shipping_address">
-                  Ship To
-                        </label>
-                <textarea class="form-control" cols="20" id="shipping_address" name="shipping_address" rows="3"></textarea>
-              </div>
-              <div class="form-group ">
-                <label class="control-label" for="email">
-                  Customer Email
-                        </label>
-                <input class="form-control" id="email" name="email" type="text" />
-              </div>
-              <div class="form-group ">
-                <label class="control-label " for="paper">
-                  Paper invoice address
-                        </label>
-                <textarea class="form-control" cols="40" id="paper" name="paper" rows="3"></textarea>
-              </div>
-              {/* <div class="form-group">
-                <label class="control-label " for="billing">
-                  Billing %
-                        </label>
-                <input class="form-control" id="billing" name="billing" type="text" />
-              </div> */}
-              <div>
-                <div class="item-form">
-                  <div class="form-group ">
-                    <label class="control-label " for="vendor">
-                      Vendor
-                                </label>
-                    <input class="form-control" id="vendor" name="vendor" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label " for="quantity">
-                      Quantity
-                                </label>
-                    <input class="form-control" id="quantity" name="quantity" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label " for="item_number">
-                      Item number
-                                </label>
-                    <input class="form-control" id="item_number" name="item_number" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label " for="description">
-                      Description
-                                </label>
-                    <input class="form-control" id="description" name="description" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label " for="unit_cost">
-                      Unit cost
-                                </label>
-                    <input class="form-control" id="unit_cost" name="unit_cost" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label ">
-                      Insurance?
-                                </label>
-                    <div class=" ">
-                      <div class="checkbox">
-                        <label class="checkbox">
-                          <input name="insurance" type="checkbox" value="Yes" />
-                                            Yes
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label " for="unit_sales_price">
-                      Unit Sales Price
-                                </label>
-                    <input class="form-control" id="unit_sales_price" name="unit_sales_price" type="text" />
-                  </div>
-                  <div class="form-group ">
-                    <label class="control-label ">
-                      Sales tax?
-                  </label>
-                    <div class=" ">
-                      <div class="checkbox">
-                        <label class="checkbox">
-                          <input name="checkbox1" type="checkbox" value="Yes" />
-                                            Yes
-                      </label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div>
+        <form
+          onSubmit={this.handleSubmit}
+        >
+          <label htmlFor="username">Username:</label>
+          <input id="username" name="username" type="text" />
 
-            </form>
-          </div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" />
+
+          <button>Send data!</button>
+        </form>
+
+        <div className="res-block">
+          <h3>Transformed data to be sent:</h3>
+          <pre>FormData {this.data}</pre>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Form;
+
+export default Form
